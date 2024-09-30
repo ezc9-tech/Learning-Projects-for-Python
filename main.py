92987e8617a1f65a2a29f62abedc2ed988cd30c8
ezc9-tech <ezc9@outlook.com>
2024-09-30 09:55:47 -0700

    "first commit and push"

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# ---------------------------- TIMER RESET ------------------------------- # 
 
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.config(padx= 100, pady= 50, bg= YELLOW)
+window.title("Pomodoro")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+canvas = Canvas(width=200, height=224, bg= YELLOW, highlightthickness= 0)
+tomato = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image= tomato)
+canvas.create_text(100, 130, text= "00:00", fill= "white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+window.mainloop()
