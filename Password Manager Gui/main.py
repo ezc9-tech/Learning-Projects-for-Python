d55c9d4050b8204722f2351e638a396211b81bb9
ezc9-tech <ezc9@outlook.com>
2024-10-01 13:57:44 -0700

    "This is a password manager"

--- Password Manager Gui/main.py
+++ Password Manager Gui/main.py
@@ -3,6 +3,16 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add():
+    password_content = password_text.get()
+    website_content = website_text.get()
+    email_content = email_text.get()
+    with open("data.txt", "a") as file:
+        file.write(f"{website_content} | {email_content} | {password_content}\n")
+        password_text.delete(0, END)
+        website_text.delete(0, END)
+    file.close()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -18,12 +28,14 @@ website = Label(text="Website:")
 website.grid(column=0, row=1)
 
 website_text = Entry(width=35)
+website_text.focus()
 website_text.grid(column=1, row=1, columnspan=2)
 
 email = Label(text="Email/Username:")
 email.grid(column=0, row=2)
 
 email_text = Entry(width=35)
+email_text.insert(0, "ezc9@outlook.com")
 email_text.grid(column=1, row=2, columnspan=2)
 
 password = Label(text="Password:")
@@ -35,7 +47,7 @@ password_text.grid(column=1, row=3)
 password_button = Button(text="Generate Password")
 password_button.grid(column=2, row=3, columnspan=2)
 
-add_button = Button(text="Add", width=36)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button = Button(text="Add", width=36, command= add)
+add_button.grid(column=1, row=4, columnspan=2,)
 
 window.mainloop()
