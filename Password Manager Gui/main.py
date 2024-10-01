939a39c0f99e358ef7b160495d39979eed63dd51
ezc9-tech <ezc9@outlook.com>
2024-10-01 13:41:52 -0700

    "Adds the counter feature and complete UI"

---
+++ Password Manager Gui/main.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.config(padx=20, pady=20)
+window.title("Password Manager")
+
+logo = PhotoImage(file=r"C:\Users\ezc9\Documents\GitHub\Learning-Projects-for-Python\Password Manager Gui\logo.png")
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas.create_image(100,100,image=logo)
+canvas.grid(column=1, row=0)
+
+website = Label(text="Website:")
+website.grid(column=0, row=1)
+
+website_text = Entry(width=35)
+website_text.grid(column=1, row=1, columnspan=2)
+
+email = Label(text="Email/Username:")
+email.grid(column=0, row=2)
+
+email_text = Entry(width=35)
+email_text.grid(column=1, row=2, columnspan=2)
+
+password = Label(text="Password:")
+password.grid(column=0, row=3)
+
+password_text = Entry(width=21)
+password_text.grid(column=1, row=3)
+
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3, columnspan=2)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
