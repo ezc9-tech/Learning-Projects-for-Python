954c59c45a03e61b2ab47f4d3755af3a07ced976
ezc9-tech <ezc9@outlook.com>
2024-10-06 17:06:14 -0500

    "This changes to gui to only have one submit button makin it simpler"

--- Money Manager/main.py
+++ Money Manager/main.py
@@ -9,52 +9,44 @@ window = Tk()
 window.config(padx=50, pady= 50, bg=blue)
 window.title("Money Manager")
 
+
 #---------------------------------Adding Money----------------------------#
 monthly_money = 0.0
 money = 0
 expenses = 0.0
 month_value = ""
-data = {"January": [0, 0],
-        "February": [0, 0],
-        "March": [0, 0],
-        "April": [0, 0],
-        "May": [0, 0],
-        "June": [0, 0],
-        "July": [0, 0],
-        "August": [0, 0],
-        "September": [0, 0],
-        "October": [0, 0],
-        "November": [0, 0],
-        "December": [0, 0]}
-file = pandas.DataFrame(data)
-rows = file.iterrows()
-print(rows)
-
-def add_money():
-    global monthly_money, file, money
-    monthly_money = float(income_grabber.get())
-    money += monthly_money
-    total_money.config(text=f"Total Money: ${money}")
-    if month_value in file.columns:
-        file.at[0, month_value] = monthly_money
-    print(file)
+data = {"January": 0,
+        "February": 0,
+        "March": 0,
+        "April": 0,
+        "May": 0,
+        "June": 0,
+        "July": 0,
+        "August": 0,
+        "September": 0,
+        "October": 0,
+        "November": 0,
+        "December": 0}
 
 
-def subtract_money():
-    global monthly_money, expenses, money
-    expenses = float(expense_grabber.get())
-    money -= expenses
-    total_money.config(text=f"Total Money: ${money}")
-    if month_value in file.columns:
-        file.at[1, month_value] = -expenses
-    print(file)
-        
+def calculate():
+    global monthly_money, money, expenses, month_value
 
-def get_month():
-    global month_value, monthly_money, expenses
-    monthly_money = 0
-    expenses = 0
     month_value = root.get()
+    monthly_money = float(income_grabber.get())
+    food = float(food_grabber.get())
+    utilities = float(utilities_grabber.get())
+    travel = float(travel_grabber.get())
+    housing = float(housing_grabber.get())
+    debt = float(debt_grabber.get())
+    insurance = float(insurance_grabber.get())
+
+    expenses = food + utilities + travel + housing + debt + insurance
+    money = monthly_money - expenses
+
+    total_money.config(text=f"Total Money: ${money}")
+    monthly_money_printer.config(text=f"Monthly Money Remaining: {monthly_money}")
+
 
 
 
@@ -65,6 +57,7 @@ def get_month():
 title = Label(text="Money Manager", fg="white", bg=blue, highlightthickness=0, font=title_font)
 title.grid(row=0, column=1)
 
+
 #what month is it
 month_title = Label(text="Month:" , fg= "white", bg=blue, highlightthickness=0, font= sub_font)
 month_title.grid(row=1)
@@ -73,8 +66,6 @@ root = StringVar(window)
 root.set(options[0])
 month = OptionMenu(window, root, *options,)
 month.grid(row=1, column =1)
-month_button = Button(text="Submit", command=get_month)
-month_button.grid(row=1, column = 2)
 
 
 #Monthly income grabber
@@ -82,28 +73,57 @@ income_title = Label(text="Monthly Income:", fg="white", bg=blue, highlightthick
 income_title.grid(row=2, column=0)
 income_grabber = Entry(width=40)
 income_grabber.grid(row=2, column=1)
-income_button = Button(text="Submit", command=add_money)
-income_button.grid(row=2, column=2)
+
 
 #Expence grabber
-expense_title = Label(text="Expenses:", fg="white", bg=blue, highlightthickness=0, font=sub_font)
-expense_title.grid(row=3, column=0)
-expense_grabber = Entry(width=40)
-expense_grabber.grid(row=3, column=1)
-expense_button = Button(text="Submit", command=subtract_money)
-expense_button.grid(row=3, column=2)
+food_title = Label(text="Food and Groceries:", fg="white", bg=blue, highlightthickness=0, font=sub_font)
+food_title.grid(row=3, column=0)
+food_grabber = Entry(width=40)
+food_grabber.grid(row=3, column=1)
+
+
+utilities_title = Label(text="Utilities:", fg="white", bg=blue, highlightthickness=0, font=sub_font)
+utilities_title.grid(row=4, column=0)
+utilities_grabber = Entry(width=40)
+utilities_grabber.grid(row=4, column=1)
+
+
+travel_title = Label(text="Travel:", fg="white", bg=blue, highlightthickness=0, font=sub_font)
+travel_title.grid(row=5, column=0)
+travel_grabber = Entry(width=40)
+travel_grabber.grid(row=5, column=1)
+
+
+housing_title = Label(text="Housing:", fg="white", bg=blue, highlightthickness=0, font=sub_font)
+housing_title.grid(row=6, column=0)
+housing_grabber = Entry(width=40)
+housing_grabber.grid(row=6, column=1)
+
+
+insurance_title = Label(text="Insurance:", fg="white", bg=blue, highlightthickness=0, font=sub_font)
+insurance_title.grid(row=7, column=0)
+insurance_grabber = Entry(width=40)
+insurance_grabber.grid(row=7, column=1)
+
+
+debt_title = Label(text="Debt:", fg="white", bg=blue, highlightthickness=0, font=sub_font)
+debt_title.grid(row=8, column=0)
+debt_grabber = Entry(width=40)
+debt_grabber.grid(row=8, column=1)
+
+
+debt_button = Button(text="Submit", command=calculate)
+debt_button.grid(row=11, column=1)
+
 
 #Total Money printer
 total_money = Label(text=f"Total Money: ${money}", bg=blue, fg="white", highlightthickness=0, font=sub_font)
-total_money.grid(row=4, column=1)
+total_money.grid(row=10, column=1)
+
 
 #Monthly Money printer
 monthly_money_printer = Label(text=f"Monthly Money Remaining: {monthly_money}", bg=blue, fg="white", highlightthickness=0, font=sub_font)
-monthly_money_printer.grid(row=5, column=1)
-
-
-
-
+monthly_money_printer.grid(row=9, column=1)
 
 
 window.mainloop()
